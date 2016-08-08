08401f4606c84e5fd2ef5b045488aa5d061e7525
Alexis Caffa <AlexisCaffa@users.noreply.github.com>
2016-08-08 04:03:37 -0300

    Open attribution link on new tab
    Add target="_blank" to link attribution item

--- src/control/Control.Attribution.js
+++ src/control/Control.Attribution.js
@@ -14,7 +14,7 @@ L.Control.Attribution = L.Control.extend({
 
 		// @option prefix: String = 'Leaflet'
 		// The HTML text shown before the attributions. Pass `false` to disable.
-		prefix: '<a href="http://leafletjs.com" title="A JS library for interactive maps">Leaflet</a>'
+		prefix: '<a href="http://leafletjs.com" title="A JS library for interactive maps" target="_blank">Leaflet</a>'
 	},
 
 	initialize: function (options) {
